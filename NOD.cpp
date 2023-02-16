579979f58fa2dfd1f9234584d749f2fd06bc99c8
qurbanolum <orchangurbanoff@gmail.com>
2023-02-17 00:40:52 +0400

    sdelal ewe paru zadach

---
+++ NOD.cpp
@@ -0,0 +1,21 @@
+//Написать рекурсивную функцию нахождения
+//наибольшего общего делителя двух целых чисел.
+#include "pch.h"
+//#include<stdlib.h>
+//#include<iostream>
+//#include<windows.h>
+//#include<time.h>
+//using namespace std;
+//
+//int NOD(int ch1, int ch2) {
+//	if (ch2 == 0) {
+//		return ch1;
+//	}
+//	else {
+//		return NOD(ch2, ch1 % ch2);
+//	}
+//}
+//
+//int main() {
+//	cout << NOD(0, 5);
+//}
--- peregrujenieFunkcii.cpp
+++ peregrujenieFunkcii.cpp
@@ -7,127 +7,122 @@ char) для выполнения следующих задач :
 ■ Сортировка элементов по возрастанию отдельно для
 каждой строки матрицы.*/
 #include "pch.h"
-#include<stdlib.h>
-#include<iostream>
-#include<windows.h>
-#include<time.h>
-using namespace std;
-//funkciya dlya int
-int matrix(int size, int massiv[5][5])
-{
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << "Vvedite element massiva [" << i << "] [" << j << "] - "; cin >> massiv[i][j];
-		}
-	}
-	int maximal = 0, minimal = 0;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << massiv[i][j] << " ";
-			maximal = ((i == j) && (maximal < massiv[i][j])) ? massiv[i][j] : maximal;
-			minimal = ((i == j) && (minimal > massiv[i][j])) ? massiv[i][j] : minimal;
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			for (int k = 0; k < size-1; k++) {
-				if (massiv[j][k] > massiv[j][k + 1]) {
-					int temp = massiv[j][k];
-					massiv[j][k] = massiv[j][k + 1];
-					massiv[j][k + 1] = temp;
-				}
-			}
-		}
-	}
-	cout << "Otsortirovannie" << endl;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << massiv[i][j] << " ";
-		}
-		cout << endl;
-	}
-	return minimal, maximal;
-}
-//funkciya dlya double
-double matrix(int size, double massiv[5][5])
-{
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << "Vvedite element massiva [" << i << "] [" << j << "] - "; cin >> massiv[i][j];
-		}
-	}
-	double maximal = 0, minimal = 0;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << massiv[i][j] << " ";
-			maximal = ((i == j) && (maximal < massiv[i][j])) ? massiv[i][j] : maximal;
-			minimal = ((i == j) && (minimal > massiv[i][j])) ? massiv[i][j] : minimal;
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			for (int k = 0; k < size - 1; k++) {
-				if (massiv[j][k] > massiv[j][k + 1]) {
-					int temp = massiv[j][k];
-					massiv[j][k] = massiv[j][k + 1];
-					massiv[j][k + 1] = temp;
-				}
-			}
-		}
-	}
-	cout << "Otsortirovannie" << endl;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << massiv[i][j] << " ";
-		}
-		cout << endl;
-	}
-	return minimal, maximal;
-}
-//не смог придумать добавление, сравнение и сортировку для типа char
-char matrix(int size, char massiv[5][5])
-{
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			char simvol;
-			cout << "Vvedite element massiva [" << i << "] [" << j << "] - "; cin >> simvol;
-			strcpy_s(&massiv[i][j],1,&simvol);
-		}
-	}
-	char maximal = 0, minimal = 0;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << massiv[i][j] << " ";
-			maximal = ((i == j) && ((int)maximal < (int)massiv[i][j])) ? massiv[i][j] : maximal;
-			minimal = ((i == j) && ((int)minimal > (int)massiv[i][j])) ? massiv[i][j] : minimal;
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			for (int k = 0; k < size - 1; k++) {
-				if ((int)massiv[j][k] > (int)massiv[j][k + 1]) {
-					int temp = massiv[j][k];
-					massiv[j][k] = massiv[j][k + 1];
-					massiv[j][k + 1] = temp;
-				}
-			}
-		}
-	}
-	cout << "Otsortirovannie" << endl;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << massiv[i][j] << " ";
-		}
-		cout << endl;
-	}
-	return minimal, maximal;
-}
-
-int main() {
-	int massiv[5][5];
-	matrix(5, massiv);
-	return 0;
-}
+//#include<stdlib.h>
+//#include<iostream>
+//#include<windows.h>
+//#include<time.h>
+//using namespace std;
+//int matrix(int size, int massiv[5][5])
+//{
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << "Vvedite element massiva [" << i << "] [" << j << "] - "; cin >> massiv[i][j];
+//		}
+//	}
+//	int maximal = 0, minimal = 0;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << massiv[i][j] << " ";
+//			maximal = ((i == j) && (maximal < massiv[i][j])) ? massiv[i][j] : maximal;
+//			minimal = ((i == j) && (minimal > massiv[i][j])) ? massiv[i][j] : minimal;
+//		}
+//		cout << endl;
+//	}
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			for (int k = 0; k < size-1; k++) {
+//				if (massiv[j][k] > massiv[j][k + 1]) {
+//					int temp = massiv[j][k];
+//					massiv[j][k] = massiv[j][k + 1];
+//					massiv[j][k + 1] = temp;
+//				}
+//			}
+//		}
+//	}
+//	cout << "Otsortirovannie" << endl;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << massiv[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//	return minimal, maximal;
+//}
+//double matrix(int size, double massiv[5][5])
+//{
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << "Vvedite element massiva [" << i << "] [" << j << "] - "; cin >> massiv[i][j];
+//		}
+//	}
+//	double maximal = 0, minimal = 0;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << massiv[i][j] << " ";
+//			maximal = ((i == j) && (maximal < massiv[i][j])) ? massiv[i][j] : maximal;
+//			minimal = ((i == j) && (minimal > massiv[i][j])) ? massiv[i][j] : minimal;
+//		}
+//		cout << endl;
+//	}
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			for (int k = 0; k < size - 1; k++) {
+//				if (massiv[j][k] > massiv[j][k + 1]) {
+//					int temp = massiv[j][k];
+//					massiv[j][k] = massiv[j][k + 1];
+//					massiv[j][k + 1] = temp;
+//				}
+//			}
+//		}
+//	}
+//	cout << "Otsortirovannie" << endl;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << massiv[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//	return minimal, maximal;
+//}
+//char matrix(int size, char massiv[5][5])
+//{
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << "Vvedite element massiva [" << i << "] [" << j << "] - "; cin >> massiv[i][j];
+//		}
+//	}
+//	char maximal = '0', minimal = '0';
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << massiv[i][j] << " ";
+//			maximal = ((i == j) && (maximal < massiv[i][j])) ? massiv[i][j] : maximal;
+//			minimal = ((i == j) && (minimal > massiv[i][j])) ? massiv[i][j] : minimal;
+//		}
+//		cout << endl;
+//	}
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			for (int k = 0; k < size - 1; k++) {
+//				if (massiv[j][k] > massiv[j][k + 1]) {
+//					int temp = massiv[j][k];
+//					massiv[j][k] = massiv[j][k + 1];
+//					massiv[j][k + 1] = temp;
+//				}
+//			}
+//		}
+//	}
+//	cout << "Otsortirovannie" << endl;
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size; j++) {
+//			cout << massiv[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//	return minimal, maximal;
+//}
+//
+//int main() {
+//	int massiv[5][5];
+//	matrix(5, massiv);
+//	return 0;
+//}
